44192388921623b16a898fb506f1472b7af8378b
John Trujillo <amriikk@gmail.com>
2025-01-02 04:20:40 -0800

    update about css bug

---
+++ src/components/Technologies.jsx
@@ -0,0 +1,9 @@
+const Technologies = () => {
+  return (
+    <div className="border-b border-neutral-800 pb-24">
+        <h2 className="my-20 text-center text-4xl">Technologies</h2>
+    </div>
+  )
+}
+
+export default Technologies
