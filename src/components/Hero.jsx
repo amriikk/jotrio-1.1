2afb86847b34e20281d0af214bbb85cc0ac16712
John Trujillo <amriikk@gmail.com>
2025-01-02 03:39:14 -0800

    finish Hero component + create About component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import { HERO_CONTENT } from '../constants'
+import profilePic from '../assets/JT_Full-Rm.png'
 
 const Hero = () => {
   return (
@@ -8,7 +9,12 @@ const Hero = () => {
                 <div className="flex flex-col items-center lg:items-start">
                     <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">Jhon Trujillo</h1>
                     <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">Full Stack Developer</span>
-                    <p>{HERO_CONTENT}</p>
+                    <p className='my-2 max-w-xl py-6 font-light tracking-tigher'>{HERO_CONTENT}</p>
+                </div>
+            </div>
+            <div className="w-full lg:w-1/2 lg:p-8">
+                <div className="flex justify-center ">
+                    <img src={profilePic} alt="Jhon Trujillo" />
                 </div>
             </div>
         </div>
