1730bc5e712fc6121850bda3fe267e569c79c426
John Trujillo <amriikk@gmail.com>
2025-01-02 01:42:45 -0800

    update bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline"> Hello world! </h1>
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
     </div>
   );
 };
