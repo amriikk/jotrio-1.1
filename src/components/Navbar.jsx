13c86db5ef406ccd61fb7c75a77a1605056816a3
John Trujillo <amriikk@gmail.com>
2025-01-02 03:32:38 -0800

    create Hero component + update Hero Section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,20 @@
-import logo from "../assets/jotrLogo.png"
+import logo from "../assets/JtLogo.png";
+import { FaGithub} from "react-icons/fa";
+import { FaLinkedin} from "react-icons/fa";
+import { FaInstagram} from "react-icons/fa";
+import { FaSquareXTwitter} from "react-icons/fa6";
 
 const Navbar = () => {
   return (
     <nav className="mb-20 flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center">
-            <img src= {logo} alt="Portfolio logo" />
+            <img className= "mx-2 w-10" src= {logo} alt="Portfolio logo" />
+        </div>
+        <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+            <FaGithub />
+            <FaLinkedin />
+            <FaInstagram />
+            <FaSquareXTwitter />
         </div>
     </nav>
   );
