cf5807fe751d668a44f2b6f98ae2fa0ca0fac56b
John Trujillo <amriikk@gmail.com>
2025-01-03 00:47:29 -0800

    add more animation for tech stack

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -25,7 +25,11 @@ const Technologies = () => {
   return (
     <div className="border-b border-neutral-800 pb-24">
         <h2 className="my-20 text-center text-4xl">Technologies</h2>
-        <div className="flex flex-wrap items-center justify-center gap-4">
+        <motion.div 
+          whileInView={{ opacity: 0, x: 0 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 1.5 }}
+          className="flex flex-wrap items-center justify-center gap-4">
             <motion.div 
               variants={iconVariants(2.5)}
               initial="initial"
@@ -88,7 +92,7 @@ const Technologies = () => {
               className="rounded-2xl border-4 border-neutral-800 p-4">
             <SiBootstrap className='text-7xl text-blue-900'/>
             </motion.div>
-        </div>
+        </motion.div>
     </div>
   )
 }
