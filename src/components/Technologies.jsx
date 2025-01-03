17bb0e84bf8c81ff8b1aa74e454ffe3bdbe11cf4
John Trujillo <amriikk@gmail.com>
2025-01-03 00:57:44 -0800

    fixed bug not displaying the tech stack with appropriate animations

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -16,7 +16,7 @@ const iconVariants = (duration) => ({
       duration: duration,
       ease: "linear",
       repeat: Infinity,
-      repeatType: "reverse"
+      repeatType: "reverse",
     }
   }
 })
@@ -24,12 +24,20 @@ const iconVariants = (duration) => ({
 const Technologies = () => {
   return (
     <div className="border-b border-neutral-800 pb-24">
-        <h2 className="my-20 text-center text-4xl">Technologies</h2>
-        <motion.div 
-          whileInView={{ opacity: 0, x: 0 }}
-          initial={{ opacity: 0, x: -100 }}
+        <motion.h2 
+          whileInView={{ opacity: 1, y: 0}}
+          initial={{ opacity: 0, y: -100 }}
           transition={{ duration: 1.5 }}
-          className="flex flex-wrap items-center justify-center gap-4">
+          className="my-20 text-center text-4xl"
+        >
+          Technologies
+        </motion.h2>
+            <motion.div 
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: -100 }}
+              transition={{ duration: 1.5 }}
+              className="flex flex-wrap items-center justify-center gap-4"
+            >
             <motion.div 
               variants={iconVariants(2.5)}
               initial="initial"
