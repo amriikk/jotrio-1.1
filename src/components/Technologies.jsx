770767c3112eec3d5789430b07aa1c4e07363f08
John Trujillo <amriikk@gmail.com>
2025-01-03 00:42:35 -0800

    add motion to all tech icon in different variances

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -8,35 +8,86 @@ import { SiTailwindcss } from "react-icons/si";
 import { SiBootstrap } from "react-icons/si";
 import { motion } from "framer-motion";
 
+const iconVariants = (duration) => ({
+  initial: { y: -10},
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse"
+    }
+  }
+})
+
 const Technologies = () => {
   return (
     <div className="border-b border-neutral-800 pb-24">
         <h2 className="my-20 text-center text-4xl">Technologies</h2>
         <div className="flex flex-wrap items-center justify-center gap-4">
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            <motion.div 
+              variants={iconVariants(2.5)}
+              initial="initial"
+              animate="animate"
+              className="rounded-2xl border-4 border-neutral-800 p-4"
+            >
             <RiReactjsLine className='text-7xl text-cyan-400'/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div
+              variants={iconVariants(1.5)}
+              initial="initial"
+              animate="animate"
+              className="rounded-2xl border-4 border-neutral-800 p-4"
+            >
             <TbBrandNextjs className='text-7xl'/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div 
+              variants={iconVariants(1)}
+              initial="initial"
+              animate="animate"
+              className="rounded-2xl border-4 border-neutral-800 p-4"
+            >
             <SiMongodb className='text-7xl text-green-400'/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div 
+              variants={iconVariants(2.5)}
+              initial="initial"
+              animate="animate"
+              className="rounded-2xl border-4 border-neutral-800 p-4"
+            >
             <DiRedis className='text-7xl text-red-700'/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div
+              variants={iconVariants(1.5)}
+              initial="initial"
+              animate="animate"
+              className="rounded-2xl border-4 border-neutral-800 p-4"
+            >
             <FaNodeJs className='text-7xl text-green-500'/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div
+              variants={iconVariants(3)}
+              initial="initial"
+              animate="animate"
+              className="rounded-2xl border-4 border-neutral-800 p-4"
+            >
             <BiLogoPostgresql className='text-7xl text-sky-700'/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div 
+              variants={iconVariants(2)}
+              initial="initial"
+              animate="animate"
+              className="rounded-2xl border-4 border-neutral-800 p-4">
             <SiTailwindcss className='text-7xl text-blue-400'/>
-            </div>
-            <div className="rounded-2xl border-4 border-neutral-800 p-4">
+            </motion.div>
+            <motion.div
+              variants={iconVariants(1.5)}
+              initial="initial"
+              animate="animate"
+              className="rounded-2xl border-4 border-neutral-800 p-4">
             <SiBootstrap className='text-7xl text-blue-900'/>
-            </div>
+            </motion.div>
         </div>
     </div>
   )
