133ff3cf1663ffdf804926b88918eba83bcd8ee8
John Trujillo <amriikk@gmail.com>
2025-01-02 23:44:04 -0800

    update constants address

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,13 @@
+import { CONTACT } from "../constants"
+
 const Contact = () => {
   return (
     <div className="border-b border-neutral-900 pb-20">
         <h2 className="my-10 text-center text-4xl">Get in Touch</h2>
+        <div className="text-center tracking-tigher">
+            <p className="my-4">{ CONTACT.address}</p>
+            <p className="my-4">{ CONTACT.phoneNo}</p>
+        </div>
     </div>
   )
 }
--- src/constants/index.js
+++ src/constants/index.js
@@ -74,7 +74,7 @@ export const PROJECTS = [
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "Santa Barbara, CA 93101 ",
+  phoneNo: "+1 646 580 2349 ",
+  email: "trujillojohnk@me.com",
 };
