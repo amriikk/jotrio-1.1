2cc2d3cdcae48b96a8cbc1fec56c40c6a835f848
John Trujillo <amriikk@gmail.com>
2025-01-15 17:50:35 -0800

    update NavBar component & rm x' git push

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,6 @@ import logo from "../assets/JtLogo.png";
 import { FaGithub} from "react-icons/fa";
 import { FaLinkedin} from "react-icons/fa";
 import { FaInstagram} from "react-icons/fa";
-import { FaSquareXTwitter} from "react-icons/fa6";
 
 const Navbar = () => {
   return (
@@ -11,10 +10,9 @@ const Navbar = () => {
             <img className= "mx-2 w-16" src= {logo} alt="Portfolio logo" />
         </div>
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-            <FaGithub />
-            <FaLinkedin />
-            <FaInstagram />
-            <FaSquareXTwitter />
+            <a href="https://github.com/amriikk"><FaGithub /></a>
+            <a href="https://www.linkedin.com/in/jhonkhrizhtian/"><FaLinkedin /></a>
+            <a href="#"><FaInstagram /></a>
         </div>
     </nav>
   );
